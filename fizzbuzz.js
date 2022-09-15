fb38c924e02ce2830589423ce897a1208c8e427a
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-15 11:10:26 -0500

    Adjustments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,10 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let num1 = Number(prompt("Enter any number 1-100: "))
 
-if (num1 % 15 == 0){
+if (num1 % 15 === 0){
     console.log("FizzBuzz!")
-}else if(num1 % 3 == 0){
+}else if(num1 % 3 === 0){
     console.log("Fizz")
-}else if(num1 % 5 == 0){
+}else if(num1 % 5 === 0){
     console.log("Buzz")
 }
