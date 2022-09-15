11158dab8b2ace48eeb17c8f475927f6a7715cb4
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-14 20:03:05 -0500

    Completed Assignment

--- space-boxing.js
+++ space-boxing.js
@@ -1,1 +1,19 @@
 const prompt = require('prompt-sync')({sigint: true});
+
+let w = Number(prompt("Enter your Earth weight: "))
+
+let p = Number(prompt("Enter Planet number: "))
+
+if(p === 1){
+    console.log(w * 0.78)
+}else if(p === 2){
+    console.log(w * 0.39)
+}else if(p === 3){
+    console.log(w * 2.65)
+}else if(p === 4){
+    console.log(w * 1.17)
+}else if(p === 5){
+    console.log(w * 1.05)
+}else if(p === 6){
+    console.log(w * 1.23)
+}
