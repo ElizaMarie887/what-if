d741a4daca78c0202eb1d056170f12fe2204a7c6
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-15 10:24:09 -0500

    Adjustments

--- space-boxing.js
+++ space-boxing.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let w = Number(prompt("Enter your Earth weight: "))
 
-let p = Number(prompt("Enter Planet number: "))
+let p = Number(prompt("Enter Planet number. 1. Venus, 2. Mars, 3. Jupiter, 4. Saturn, 5. Uranus, 6. Neptune: "))
 
 if(p === 1){
     console.log(w * 0.78)
