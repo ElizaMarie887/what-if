cf879233a90429194244919d894f4030ea47c3de
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-14 19:56:41 -0500

    Completed Assignment

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,11 @@
 const prompt = require('prompt-sync')({sigint: true});
+
+let num1 = Number(prompt("Enter any number 1-100: "))
+
+if (num1 % 15 == 0){
+    console.log("FizzBuzz!")
+}else if(num1 % 3 == 0){
+    console.log("Fizz")
+}else if(num1 % 5 == 0){
+    console.log("Buzz")
+}
