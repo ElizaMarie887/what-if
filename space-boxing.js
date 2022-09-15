7c9b3137c8813c834ef03091920dfcbcf4b65d53
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-15 10:36:56 -0500

    Adjustments for clarity

--- space-boxing.js
+++ space-boxing.js
@@ -5,15 +5,21 @@ console.log("Enter Planet number. 1. Venus, 2. Mars, 3. Jupiter, 4. Saturn, 5. U
 let p = Number(prompt("> "))
 
 if(p === 1){
+    console.log("You picked Venus. Your weight needs to be:")
     console.log(w * 0.78)
 }else if(p === 2){
+    console.log("You picked Mars. Your weight needs to be:")
     console.log(w * 0.39)
 }else if(p === 3){
+    console.log("You picked Jupiter. Your weight needs to be:")
     console.log(w * 2.65)
 }else if(p === 4){
+    console.log("You picked Saturn. Your weight needs to be:")
     console.log(w * 1.17)
 }else if(p === 5){
+    console.log("You picked Uranus. Your weight needs to be:")
     console.log(w * 1.05)
 }else if(p === 6){
+    console.log("You picked Neptune. Your weight needs to be: ")
     console.log(w * 1.23)
 }
